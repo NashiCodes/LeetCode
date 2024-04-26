fd8fd7b87138f7ff242e038b21be7d6d7ae7e331
Perera Dev <nashi.codes@gmail.com>
2024-04-25 23:51:58 -0300

    Add .gitignore file and MergeSortedArray program in C#

---
+++ Merge Sorted Array/C#/MergeSortedArray/MergeSortedArray/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MergeSortedArray;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var nums1 = new[] {1, 2, 3, 0, 0, 0};
+        var m = 3;
+        var nums2 = new[] {2, 5, 6};
+        var n = 3;
+
+        if (n == 0) return;
+        int lst = m+n-1;
+
+        while (m > 0 && n > 0)
+        {
+            if (nums1[m - 1] > nums2[n - 1])
+            {
+                nums1[lst--] = nums1[--m];
+            }
+            else
+            {
+                nums1[lst--] = nums2[--n];
+            }
+        }
+
+        while (n > 0)
+        {
+            nums1[lst--] = nums2[--n];
+        }
+    }
+}
